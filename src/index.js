9af48a3f7119bebc7bd27276c7c9fb3ba4f12bf5
Katherine Taylor <katherine.j.taylor.22@dartmouth.edu>
2020-04-15 16:09:20 -0400

    adding src and package and webpack files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('starting up!');
+/* eslint-disable linebreak-style */
+// const $ = require('jquery');
+import $ from 'jquery';
+import './style.scss';
+
+let sec = 0;
+
+function seconds() {
+  sec += 1;
+  console.log('function called');
+  $('#main').html(`You have been here for ${sec} seconds`);
+}
+
+setInterval(seconds, 1000);
