96086581bb9ddfc7650c43369a54c4bfb88edef2
Katherine Taylor <katherine.j.taylor.22@dartmouth.edu>
2020-04-19 14:50:27 -0400

    first react changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 /* eslint-disable linebreak-style */
 // const $ = require('jquery');
-import $ from 'jquery';
+// import $ from 'jquery';
+// import './style.scss';
+
+// let sec = 0;
+
+// function seconds() {
+//   sec += 1;
+//   console.log('function called');
+//   $('#main').html(`You have been here for ${sec} seconds`);
+// }
+
+// setInterval(seconds, 1000);
+
+// ^^^ old
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let sec = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-function seconds() {
-  sec += 1;
-  console.log('function called');
-  $('#main').html(`You have been here for ${sec} seconds`);
-}
-
-setInterval(seconds, 1000);
+ReactDOM.render(<App />, document.getElementById('main'));
